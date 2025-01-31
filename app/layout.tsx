6127d3efbc3fda892dfaf3ba16d0678a7c5c28eb
kimonda <bwanamaina2010@gmail.com>
2025-01-31 23:02:08 +0300

    layout changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "./components/header";
 import Footer from "./components/footer";
-import { Main } from "next/document";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
