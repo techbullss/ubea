71422b0741caae4a74cd04d1f27d7ff3da564811
kimonda <bwanamaina2010@gmail.com>
2025-02-07 19:14:45 +0300

    xxxx

--- app/aboutcomponents/section1.tsx
+++ app/aboutcomponents/section1.tsx
@@ -5,11 +5,11 @@ const section1 = () => {
   return (
     <div className='relative w-full h-screen  bg-[#fbf6fa] bg-gradient-to-r from-pink-200 to-sky-200 text-gray-900'>
         <div className=''>
-            <div className='pt-40 flex flex-col items-center '>
+            <div className='pt-28 flex flex-col items-center '>
                 <h1 className='pb-8 text-center text-3xl font-bold'>Who are we?</h1>
                 <h1 className='text-6xl font-bold text-center '> United Borders in <br></br>Education Association (UBEA)</h1>
                 <h1>Explore our range of design subscriptions, available for everyone! Enjoy the flexibility to pause or cancel anytime, with absolutely no commitments required.</h1>
-                <button className="flex justify-center items-center gap-2 mt-10 px-6 py-3 bg-blue-600 hover:bg-blue-700 text-white text-lg font-semibold rounded-full shadow-md transition duration-300 ease-in-out transform hover:scale-105">
+                <button className="flex justify-center items-center gap-2 mt-5 px-6 py-3 bg-blue-600 hover:bg-blue-700 text-white text-lg font-semibold rounded-full shadow-md transition duration-300 ease-in-out transform hover:scale-105">
                   Join UBEA
                   <FaArrowRight className="text-white text-xl transition-transform duration-300 group-hover:translate-x-1" />
                 </button>
@@ -18,7 +18,7 @@ const section1 = () => {
         </div>
         
         <div className="absolute bottom-0 w-full">
-    <svg viewBox="0 0 1440 320" xmlns="http://www.w3.org/2000/svg">
+    <svg viewBox="0 0 1300 320" xmlns="http://www.w3.org/2000/svg">
       <path
         fill="#ffffff"
         fillOpacity="1"
