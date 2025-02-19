47197762e9af7c5dbc3483a8652e55d98acdd5f8
kimonda <bwanamaina2010@gmail.com>
2025-02-19 12:51:21 +0300

    changes

--- app/components/Homepagepricing.tsx
+++ app/components/Homepagepricing.tsx
@@ -43,13 +43,13 @@ const Homepagepricing = () => {
   ];
 
   return (
-    <div className="p-2 flex flex-col">
+    <div className="p-2 flex flex-col gap-3">
     <h2 className="text-2xl font-bold text-blue-600 mb-4 text-center">
       Benefits of UBEA Membership (included in Annual Membership)
     </h2>
   
     {/* Flex container for table and image */}
-    <div className="grid grid-cols-2  gap-0">
+    <div className="grid grid-cols-2  gap-5">
       
       {/* Table Section - Adjusted Width */}
       <div className="w-[45%]">
