71987c40f73183aab4e480176d296913b3b30e8b
kimonda <bwanamaina2010@gmail.com>
2025-02-24 13:51:24 +0300

    changes

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -65,7 +65,7 @@ return ()=>{
         </div>
         
         <div className='flex items-center gap-3'>
-          <button className='bg-white rounded-full md:px-8 md:py-2.5'>login</button>
+          <button className='bg-white rounded-full px-8  py-2.5 md:px-8 md:py-2.5'>login</button>
           <HiBars3BottomRight className=' w-8 cursor-pointer text-white h-8 lg:hidden ' onClick={() => setIsOpen(true)}/>
         </div>
     </div>
