b404abfce98c9063a4ba30cd8e91386147dd4835
kimonda <bwanamaina2010@gmail.com>
2025-03-01 19:53:08 +0300

    ccvvcv

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -5,7 +5,7 @@ import Nav from './Nav'
 
 const header = () => {
   return (
-    <div className='md:h-[12hv] h-[6hv]'>
+    <div className='md:h-[12hv] h-[8hv]'>
         <Nav />
         </div>  
   )
