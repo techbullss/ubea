92fc400f7a9bccbe95f090a66d3d4bfc38ff169d
kimonda <bwanamaina2010@gmail.com>
2025-03-02 12:06:03 +0300

    sssss

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
       >
         <div className="flex flex-col h-screen  ">
       <Header />
-      <main className="flex-grow  mt-12 md:mt-16">{children}</main> 
+      <main className="flex-grow  mt-10 md:mt-16">{children}</main> 
       <Footer /> 
     </div>
       </body>
