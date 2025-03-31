d020858bbd115a69c5eb747f393235bc8b47d2f0
kimonda <bwanamaina2010@gmail.com>
2025-03-31 09:03:42 +0300

    main

--- app/components/WhatsAppButton.tsx
+++ app/components/WhatsAppButton.tsx
@@ -3,7 +3,7 @@ import { FaWhatsapp } from "react-icons/fa";
 export default function WhatsAppButton() {
   return (
     <a
-      href="https://wa.me/+1443653-8482" 
+      href="https://wa.me/14436538482" 
       target="_blank"
       rel="noopener noreferrer"
       className="md:fixed md:bottom-6 md:right-6 fixed right-[50%] bottom-6 bg-green-500 text-white p-4 rounded-full shadow-lg hover:bg-green-600 transition duration-300 flex items-center justify-center"
